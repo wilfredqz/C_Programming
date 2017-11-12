c6e987e2a3906723bedc6ebd34f0e8ccfb504cac
slow_walker <wqz1231@gmail.com>
2017-11-12 13:13:44 +0100

    my second commit by adding functions

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
 print ('Hello world!')
+
+import numpy as np
+r = 5
+def f(r):
+    cir = 2*np.pi * r
+    print ('circumrance =',cir)
+def g(r):
+    area = np.pi* np.power(r,2)
+    print ('area =',area)
+
+f(r)
+g(r)
